d10cd9ff838bc8724d4a6cee767f083bad2b705e
skillacademyonwheels <advelestica.design@gmail.com>
2025-09-02 12:33:33 +0530

    v1

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+# Joke API endpoint
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+
+# Send GET request to fetch a joke
+
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    joke = response.json() 
+    print(f"{joke['setup']} - {joke['punchline']}")
+else:
+    print("Failed to retrieve a joke.")
